af35491cffa22bc8a616ba1da65fba429defa2cc
Yuanuo <afeinet@gmail.com>
2019-08-29 15:02:56 +0800

    Add Chinese language support to CodeMirror.

---
+++ modules/org.opencms.base/static/editors/codemirror/js/lang-zh.js
@@ -0,0 +1,12 @@
+﻿// chinese localization of editor dialogs
+var lang = {
+  search : "搜索：",
+  hint : "使用 /re/ 标记进行正则表达式搜索",
+  replace : "替换：",
+  replacewith : "替换为：",
+  replaceconfirm : "确认替换？",
+  replaceyes : "是",
+  replaceno : "否",
+  replacestop : "停止",
+  fontsize: "--字体大小--"
+};
